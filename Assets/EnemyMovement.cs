e72850996c6c2bf4f722542c2b2daa877ec0f5be
tmorris37 <tmorris3@uw.edu>
2024-10-20 17:57:38 -0700

    Started work on Enemy Movement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

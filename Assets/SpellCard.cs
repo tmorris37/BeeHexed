d49ee785290de09dd8e28405473647dde374f313
Cole VanLeer <cvanleer99@gmail.com>
2024-11-01 12:05:31 -0700

    Enabled cards to be read from JSON files

--- Assets/SpellCard.cs
+++ Assets/SpellCard.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
 
 [CreateAssetMenu(fileName = "New Spell Card", menuName = "Card/Spell Card")]
 public class SpellCard : Card
 {
-    [SerializeField] private SpellType spellType;
-    enum SpellType {
+  [SerializeField] private int SpellID;
+  private SpellData SpellData;
+
+  void Start() {
+    var FileData = Resources.Load<TextAsset>("Spells/Spell_" + SpellID);
+
+        if (FileData != null) {
+            string JSONPlainText = FileData.text;
+            this.SpellData = JsonConvert.DeserializeObject<SpellData>(JSONPlainText);
+        }
+        else {
+            Debug.Log("Unable to load Spell_" + SpellID);
+        }
+  }
+}
+
+
+public class SpellData {
+
+  public enum SpellType {
       Blessing,
       Hex
     }
+  public SpellType Type{ get; set;}
+  public int Damage{ get; set;}
+  public int AttackSpeed{ get; set;}
 }
--- Assets/TowerCard.cs
+++ Assets/TowerCard.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using Newtonsoft.Json;
 
 [CreateAssetMenu(fileName = "New Tower Card", menuName = "Card/Tower Card")]
 public class TowerCard : Card
 {
   [SerializeField] private int TowerID;
-  private int health;
-  private int damage;
-  private double attackSpeed;
+  private TowerData TowerData;
+
+  void Start() {
+    var FileData = Resources.Load<TextAsset>("Towers/Tower_" + TowerID);
+
+        if (FileData != null) {
+            string JSONPlainText = FileData.text;
+            this.TowerData = JsonConvert.DeserializeObject<TowerData>(JSONPlainText);
+        }
+        else {
+            Debug.Log("Unable to load Tower_" + TowerID);
+        }
+  }
+}
+
+
+public class TowerData {
+  public int MaxHealth{ get; set;}
+  public int Damage{ get; set;}
+  public int AttackSpeed{ get; set;}
 }
 

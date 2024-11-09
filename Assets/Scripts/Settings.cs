5ef0f84ac7706d3cbe0f47e39e9b7ca1f50151ef
Colin <kxiaocolin@gmail.com>
2024-11-09 03:06:45 -0800

    UI added to Settings
    Running into bug with TMPro dropdown

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,25 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
+
 public class Settings : MonoBehaviour
 {
-    [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private Slider volumeSlider;
-
-    // Set Volume
-    public void SetVolume()
+    public void SetQuality (int qualityIndex)
     {
-        float volume = volumeSlider.value;
-        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
-    }
-
-    // Brings User to previous scene
-    public void GoBack(string path)
-    {
-        // Hard Coded to bring back to MainMenu
-        SceneManager.LoadScene("MainMenu");
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 }

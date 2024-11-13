8ba1301ab0708a0a71f7740bf2a6dc322492e080
Colin <kxiaocolin@gmail.com>
2024-11-12 20:22:33 -0800

    Added Button functionality to back button in settings

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class Settings : MonoBehaviour
 {
+
+    // Sets quality of 
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    // Brings User to previous scene
+    public void GoBack(string path)
+    {
+        // Hard Coded to bring back to MainMenu
+        SceneManager.LoadScene("MainMenu");
     }
 }

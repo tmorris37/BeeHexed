83bd546a8c3f5ef19ab4097f789f27807a2e785d
EliHess <elihess14@gmail.com>
2024-11-29 13:31:38 -0800

    Minor tweaks to things
    -Settings screen tweaks
    -Enemy tweaks

--- Assets/Scripts/Enemies And Towers/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies And Towers/Enemies/Enemy.cs
@@ -68,7 +68,7 @@ namespace EnemyAndTowers
             this.health = this.data.MaxHealth;
             this.movementSpeed = this.data.Speed;
             this.attackDamage = this.data.Attacks[0].DamageAmount;
-            this.attackRate =  this.data.Attacks[0].AttackRate;
+            this.attackRate =  1 / this.data.Attacks[0].AttackRate;
             attackCooldown = attackRate;
 
             inertiaDir = InertiaDirection.nullDir;

b33466a68bd0e2d50f3872b08c6dd95b3a5fc180
Cole VanLeer <cvanleer99@gmail.com>
2024-12-07 21:33:36 -0800

    Bug Fixes

--- Assets/Scripts/Enemies And Towers/Towers/FireballExplosion.cs
+++ Assets/Scripts/Enemies And Towers/Towers/FireballExplosion.cs
@@ -12,13 +12,20 @@ public class FireballExplosion : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-            if (collision.CompareTag("EnemyBody") && !damaged) {
-                if (SFXManager.Instance != null) {
-                    SFXManager.Instance.PlayFireballExplode();
-                }
-                collision.gameObject.GetComponentInParent<Enemy>().TakeDamage(damage);
-                // this object lingers for the animation, but we don't actually want it to damage if an enemy enters the explosion after instantiated
-                GetComponent<CircleCollider2D>().enabled = false;
+        if (collision.CompareTag("EnemyBody") && !damaged) {
+            if (SFXManager.Instance != null) {
+                SFXManager.Instance.PlayFireballExplode();
             }
+            collision.gameObject.GetComponentInParent<Enemy>().TakeDamage(damage);
+            // this object lingers for the animation, but we don't actually want it to damage if an enemy enters the explosion after instantiated
+            GetComponent<CircleCollider2D>().enabled = false;
+            StartCoroutine(EndAnimation());
         }
+    }
+
+    private IEnumerator EndAnimation() {
+        yield return new WaitForSeconds(5);
+        End();
+    }
+
 }

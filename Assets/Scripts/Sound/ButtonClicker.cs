9a25b6493ec1e128b6eb2ec1e472a25e1522850a
Cole VanLeer <cvanleer99@gmail.com>
2024-12-17 15:49:42 -0800

    Button clicks added to every button

--- Assets/Scripts/Sound/ButtonClicker.cs
+++ Assets/Scripts/Sound/ButtonClicker.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ButtonClicker : MonoBehaviour {
     public void ButtonClickSFX() {
-        SFXManager.Instance.PlayButtonClick();
+        if (SFXManager.Instance != null) {
+            SFXManager.Instance.PlayButtonClick();
+        }
     }
 }
